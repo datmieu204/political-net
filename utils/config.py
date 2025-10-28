d76b92d04d7a7950d4cdadd7ba3f96d0da5aa43b
Sypuncute2k4 <120903694+datmieu204@users.noreply.github.com>
2025-10-29 01:05:20 +0700

    chore: modify output directory and settings paths
    Updated OUTPUT_DIR and related file paths in Settings class.

--- utils/config.py
+++ utils/config.py
@@ -1,16 +1,23 @@
 # ./utils/config.py
 
 class Settings():
-    OUTPUT_DIR = "./data/processed"
+    OUTPUT_DIR = "./data"
 
     # Crawl names settings
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     URL_CRAWL_NAME = "https://vi.wikipedia.org/wiki/Ban_Chấp_hành_Trung_ương_Đảng_Cộng_sản_Việt_Nam_khóa_"
-    OUTPUT_DIR_CRAWL_NAMES = OUTPUT_DIR + "/politicians_names.txt"
+    OUTPUT_DIR_CRAWL_NAMES = OUTPUT_DIR + ""
+    "/politicians_names.txt"
 
     # Crawl infobox settings
     VIWIKI_XML_DUMP_DIR = "data/raw/viwiki-latest-pages-articles.xml"
     INPUT_DIR_CRAWL_INFOBOX = OUTPUT_DIR_CRAWL_NAMES
-    OUTPUT_DIR_CRAWL_INFOBOX = OUTPUT_DIR + "/politicians_infoboxes.json"
+    OUTPUT_DIR_POLITICIAN_DB = OUTPUT_DIR + "/database/politicians_db.json"
+    
+    # Neo4j settings
+    NEO4J_URI = "bolt://localhost:7687"
+    NEO4J_USER = "neo4j"
+    NEO4J_PASSWORD = "12345678"
+    NEO4J_DATABASE = "neo4j"
 
 settings = Settings()
