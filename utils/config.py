c0b810a768de70f7bab934815ae3a1df234e62f0
meo <datmieu2004@gmail.com>
2025-10-30 01:35:46 +0700

    chore: update file path

--- utils/config.py
+++ utils/config.py
@@ -14,6 +14,16 @@ class Settings():
     INPUT_DIR_CRAWL_INFOBOX = OUTPUT_DIR_CRAWL_NAMES
     OUTPUT_DIR_POLITICIAN_DB = OUTPUT_DIR + "/database/politicians_db.json"
     
+    INPUT_SEED_TITLES_FILE = OUTPUT_DIR + "/mess/seed_politicians.txt"
+    INPUT_POLITICIAN_DB_FILE = OUTPUT_DIR_POLITICIAN_DB
+    OUTPUT_POLITICIAN_INFOBOX = OUTPUT_DIR + "/processed/infobox/politicians_data.json"
+
+    # Preprocessing settings
+    INPUT_CLEAN_INFOBOX_FILE = OUTPUT_POLITICIAN_INFOBOX
+    OUTPUT_CLEAN_INFOBOX_FILE = OUTPUT_DIR + "/processed/infobox/politicians_data_cleaned.json"
+    INPUT_PROVINCE_NORMALIZED_FILE = OUTPUT_CLEAN_INFOBOX_FILE
+    OUTPUT_PROVINCE_NORMALIZED_FILE = OUTPUT_DIR + "/processed/infobox/politicians_data_provinces_normalized.json"
+
     # Neo4j settings
     NEO4J_URI = "bolt://localhost:7687"
     NEO4J_USER = "neo4j"
