be7043e44ec1add79a05a1977f1e9c4a046a629b
meo <datmieu2004@gmail.com>
2025-11-18 14:08:11 +0700

    chore: change config output path

--- utils/config.py
+++ utils/config.py
@@ -6,8 +6,7 @@ class Settings():
     # Crawl names settings
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     URL_CRAWL_NAME = "https://vi.wikipedia.org/wiki/Ban_Chấp_hành_Trung_ương_Đảng_Cộng_sản_Việt_Nam_khóa_"
-    OUTPUT_DIR_CRAWL_NAMES = OUTPUT_DIR + ""
-    "/politicians_names.txt"
+    OUTPUT_DIR_CRAWL_NAMES = OUTPUT_DIR + "/politicians_names.txt"
 
     # Crawl infobox settings
     VIWIKI_XML_DUMP_DIR = "data/raw/viwiki-latest-pages-articles.xml"
